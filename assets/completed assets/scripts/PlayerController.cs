014cc5a6dd5aa458953d3ec71f6fcd0fa22d8688
Govind B Chandran <govindchandran235@gmail.com>
2020-07-26 14:01:01 +0530

    Add files via upload

---
+++ assets/completed assets/scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{ public float speed; 
+  public Text countText;
+
+  private Rigidbody2D rb2d;
+  private int count;
+  public Text WinText;
+
+  void Start(){
+  	  rb2d = GetComponent<Rigidbody2D> ();
+	  count = 0;
+	  WinText.text = "";
+	  SetCountText ();
+  }
+
+void FixedUpdate() {
+	float moveHorizontal = Input.GetAxis ("Horizontal");
+	float moveVertical = Input.GetAxis ("Vertical");
+	Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+	rb2d.AddForce (movement * speed);
+
+}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("PickUp"))
+		{	other.gameObject.SetActive(false);
+			count = count+5;
+			SetCountText();
+
+		}
+	}
+
+	void SetCountText() {
+	countText.text = "Count: " + count.ToString();
+	if (count>=80){
+		WinText.text = "YOU WIN";
+	}
+	
+	}
+
+}
---
+++ assets/completed assets/scripts/rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour
+{
+  
+    void Update()
+    { transform.Rotate (new Vector3 (0,0,45) * Time.deltaTime);
+        
+    }
+}
